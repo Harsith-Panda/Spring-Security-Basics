45b79c4c24068d7798ae24699e027425a8198cde
Harsith Priyan Sivanandham <harsithpriyan@gmail.com>
2025-06-16 12:22:22 +0530

    Implemented In-Memory Authentication.

--- src/main/java/com/example/securitydemo/config/SecurityConfig.java
+++ src/main/java/com/example/securitydemo/config/SecurityConfig.java
@@ -2,23 +2,51 @@ package com.example.securitydemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         // Below makes sure that all requests made are authenticated.
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
-        http.formLogin(withDefaults());
+        //http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user1 = User
+                .withDefaultPasswordEncoder()
+                .username("dev")
+                .password("deve")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User
+                .withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admi")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, admin);
     }
 
 }
--- src/main/java/com/example/securitydemo/controller/HelloController.java
+++ src/main/java/com/example/securitydemo/controller/HelloController.java
@@ -1,12 +1,15 @@
 package com.example.securitydemo.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/")
     public String getHello(HttpServletRequest request) {
         return "Hello " + request.getSession().getId();
@@ -15,5 +18,11 @@ public class HelloController {
     @PostMapping("/")
     public String getEvil() {
         return "Welcome to the evil side!";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String chumma(HttpServletRequest req) {
+        return "Welcome Admin!" + req.getSession().getId();
     }
 }
